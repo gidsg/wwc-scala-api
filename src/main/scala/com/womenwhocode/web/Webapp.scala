f38c671d071566c4182247f5bd608fa0d257abaf
Claire Tran <c.tran83@gmail.com>
2017-05-13 13:48:14 +0100

    Adding new endpoints

--- src/main/scala/com/womenwhocode/web/Webapp.scala
+++ src/main/scala/com/womenwhocode/web/Webapp.scala
@@ -7,5 +7,11 @@ object Webapp {
   val service = HttpService {
     case GET -> Root =>
       Ok(s"Hello")
+
+    case GET -> Root / "locations" =>
+      Ok(s"locations endpoint")
+
+    case GET -> Root / "locations" / postcode =>
+      Ok(s"locations endpoint ${postcode}")
   }
 }
